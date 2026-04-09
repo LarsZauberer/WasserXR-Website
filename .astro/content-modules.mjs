2c3fb2a9b5037da5efd48ea8fd5351e2b06e55e0
LarsZauberer <wasser.ian@gmail.com>
2026-04-09 17:37:13 +0200

    Adding more documentation pages

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,10 @@
 
 export default new Map([
+["src/content/docs/getting_started/ECS/component.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting_started%2FECS%2Fcomponent.mdx&astroContentModuleFlag=true")],
+["src/content/docs/getting_started/ECS/entity.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting_started%2FECS%2Fentity.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/getting_started/ECS/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting_started%2FECS%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/getting_started/ECS/plugin.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting_started%2FECS%2Fplugin.mdx&astroContentModuleFlag=true")],
+["src/content/docs/getting_started/ECS/system.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting_started%2FECS%2Fsystem.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/getting_started/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting_started%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")]]);
 		
