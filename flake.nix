580a5b93f2c07067063cd783689c7f101097da89
LarsZauberer <wasser.ian@gmail.com>
2026-05-22 17:39:47 +0200

    Flake: Add docker build

--- flake.nix
+++ flake.nix
@@ -6,21 +6,86 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
     flake-utils.lib.eachDefaultSystem (
-      system: let
+      system:
+      let
         pkgs = nixpkgs.legacyPackages.${system};
-      in {
+        lib = nixpkgs.lib;
+
+        websitePackage = pkgs.buildNpmPackage {
+          pname = "wasserxr-website";
+          version = "0.0.1";
+          src = ./.;
+          npmDepsHash = "sha256-ESkzHAMefspauumje+GREewCwrNfMKCP31yjpfmtIMQ=";
+          nativeBuildInputs = [
+            pkgs.autoPatchelfHook
+            pkgs.pkg-config
+          ];
+          buildInputs = [
+            pkgs.vips
+            pkgs.stdenv.cc.cc.lib
+          ];
+          npmFlags = "--ignore-scripts";
+          preBuild = "autoPatchelf node_modules";
+          buildPhase = "npm run build";
+          installPhase = "cp -r dist $out";
+        };
+
+        nginxConf = pkgs.writeText "nginx.conf" ''
+          user nobody nobody;
+          events {}
+          http {
+            include ${pkgs.nginx}/conf/mime.types;
+            server {
+              listen 80;
+              root /srv/http;
+              index index.html;
+              location / {
+                try_files $uri $uri/ /index.html;
+              }
+            }
+          }
+        '';
+      in
+      {
+        packages = {
+          default = self.packages.${system}.docker;
+          docker = pkgs.dockerTools.buildLayeredImage {
+            name = "wasserxr-website";
+            tag = "latest";
+            contents = [
+              pkgs.nginx
+              pkgs.fakeNss
+            ];
+            extraCommands = ''
+              mkdir -p var/log/nginx var/cache/nginx tmp srv/http
+              cp -r ${websitePackage}/. srv/http/
+            '';
+            config = {
+              Cmd = [
+                "${pkgs.nginx}/bin/nginx"
+                "-c"
+                "${nginxConf}"
+                "-g"
+                "daemon off;"
+              ];
+              ExposedPorts."80/tcp" = { };
+            };
+          };
+        };
+
         devShells.default = pkgs.mkShell {
           name = "devShell";
 
-          buildInputs = [pkgs.nodejs];
+          buildInputs = [ pkgs.nodejs ];
 
-          shellHook = '''';
+          shellHook = "";
         };
       }
     );
