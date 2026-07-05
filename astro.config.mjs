527e35738d572f88877802d746679b362b2e8229
Ian Wasser <wasser.ian@gmail.com>
2026-07-05 13:13:57 +0200

    Website Improvements + V0.2 release (#14)
    * Setup: Rust rewrite
    
    * Setup: Remove C lang instructions
    
    * Getting Started Section overhaul
    
    * Components: Talk about Field Permissions
    
    * Plugin: Plugin Documentation to Rust rewrite
    
    * Scene: Added Scene description
    
    * Assets: Added Asset documentation
    
    * Resources: Added documentation
    
    * Setup: Formatting
    
    * Documentation: Rephrasing and Spell Checking
    
    * Logging: Rust rewrite
    
    * Console: Rust Rewrite + WasserXR-Core split
    
    * WasserXR-Core: Lots of documentation added
    
    * WasserXR-Core: Many Docs added
    
    * WasserXR-Core: Final Docs for V0.2.0
    
    * Index: Change the API Docs link
    
    * Docs: Correctness and Spell Checking
    
    * Socials: Update GitHub Link
    
    * Blog: Remove the Blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,29 +8,27 @@ export default defineConfig({
 		starlight({
 			title: 'WasserXR',
 			customCss: ['./src/styles/custom.css'],
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/LarsZauberer/TheSeed' }, {icon: "discord", label: "Discord", href: "https://discord.gg/XcjBZn5pHy"}],
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/LarsZauberer/WasserXR' }, {icon: "discord", label: "Discord", href: "https://discord.gg/XcjBZn5pHy"}],
 			sidebar: [
-				{
-					label: 'Blog',
-					items: [
-						{ slug: 'blog' },
-						{ slug: 'blog/rewriting-wasserxr-in-rust' },
-						{ slug: 'blog/welcome' },
-					],
-				},
 				{
 					label: 'Getting Started',
 					items: [
-						{ slug: 'getting_started/installation' },
 						{ slug: 'getting_started/setup' },
 						{
 							label: 'ECS',
 							autogenerate: { directory: 'getting_started/ECS' },
 						},
-						{ slug: 'getting_started/console' },
 						{ slug: 'getting_started/logging' },
 					],
 				},
+        {
+          label: 'WasserXR-Core',
+          items: [
+            {label: 'Components', autogenerate: {directory: 'wasserxr-core/components'}},
+            {label: 'Systems', autogenerate: {directory: 'wasserxr-core/systems'}},
+            {label: 'Asset Types', autogenerate: {directory: 'wasserxr-core/assets'}}
+          ]
+        }
 			],
 		}),
 	],
